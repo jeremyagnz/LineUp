a6bdd5326b7dfc31d325027e9ee251d331d2d2a2
jeremy <jeremyagnz@agnz.com>
2022-03-26 15:16:20 -0400

    Adding navbar and Liders components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/header/nav-bar/nav-bar.component';
+import { LideresComponent } from './components/lideres/lideres.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LideresComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/lideres/lideres.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lideres',
+  templateUrl: './lideres.component.html',
+  styleUrls: ['./lideres.component.css']
+})
+export class LideresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
